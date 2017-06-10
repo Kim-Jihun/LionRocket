34317ab830fc99bf39a4d2858507a82f28c8a8c7
Kim-Jihun <ssamji0529@naver.com>
2017-06-11 00:06:19 +0900

    Delete curltest.js

--- Project/Firebase/public/curltest.js
+++
@@ -1,8 +0,0 @@
-curl = require('curl');
-  // curl('https://donwork-233dd.firebaseio.com/', {VERBOSE: 1, RAW: 1}, function(err) {
-  //   console.info(this);
-  // });
-
-data = {VERBOSE: 1, RAW: 1};
-
-  curl.postJSON('https://donwork-233dd.firebaseio.com/', {"key": "string"}, function(err, response, data){});
